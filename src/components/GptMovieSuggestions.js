c6182553cadaf8152defb3b0b0e08e10e8bfd7d7
Geetha <geethasr29@gmail.com>
2024-12-31 20:44:52 +0530

    movie suggestion change

--- src/components/GptMovieSuggestions.js
+++ src/components/GptMovieSuggestions.js
@@ -8,11 +8,11 @@ const GptMovieSuggestions = () => {
   return (
     <div className="p-4 m-4 bg-black text-white bg-opacity-90">
       <div>
-        {movieNames.map((movieName) => (
+        {movieNames.map((movieName, index) => (
           <MovieList
             key={movieName}
             title={movieName}
-            movies={movieResults[0]}
+            movies={movieResults[index]}
           />
         ))}
       </div>
